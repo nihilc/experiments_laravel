87b851d5a9ea8727f4ef8086c9681e2da6a7f617
Christian <nihilist.cc@gmail.com>
2023-05-10 09:29:57 -0500

    make base migrations models factories controllers

---
+++ imsc-server-test/app/Http/Controllers/AttributeController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Attribute;
+use Illuminate\Http\Request;
+
+class AttributeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Attribute $attribute)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Attribute $attribute)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Attribute $attribute)
+    {
+        //
+    }
+}
