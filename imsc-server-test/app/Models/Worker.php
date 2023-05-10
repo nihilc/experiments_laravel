7acad7d68e73f2f86de6bafd2169b8a512594eb7
Christian <nihilist.cc@gmail.com>
2023-05-10 16:57:19 -0500

    update Worker model

--- imsc-server-test/app/Models/Worker.php
+++ imsc-server-test/app/Models/Worker.php
@@ -4,8 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Worker extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        "icard",
+        "fname",
+        "mname",
+        "lname",
+        "mlname",
+        "photo",
+        "phone",
+        "email",
+        "title",
+    ];
+    // protected $hidden = ['created_at', 'updated_at', 'deleted_at'];
+    protected $dates = ["created_at", "updated_at", "deleted_at"];
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function city()
+    {
+        return $this->belongsTo(City::class);
+    }
 }
