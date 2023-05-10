5f3f3e2a475d2dbcd0485d51d621ec7f13b49088
Christian <nihilist.cc@gmail.com>
2023-05-10 10:07:44 -0500

    fix users table migration error

--- imsc-server-test/database/migrations/2023_05_10_141342_create_users_table.php
+++ imsc-server-test/database/migrations/2023_05_10_141342_create_users_table.php
@@ -10,10 +10,7 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create("users", function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+        Schema::table("users", function (Blueprint $table) {});
     }
 
     /**
