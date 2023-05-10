fce02d6cd952d2548ba02c42d821a139ac661244
Christian <nihilist.cc@gmail.com>
2023-05-10 16:57:01 -0500

    update Worker migration

--- imsc-server-test/database/migrations/2023_05_10_141011_create_workers_table.php
+++ imsc-server-test/database/migrations/2023_05_10_141011_create_workers_table.php
@@ -4,15 +4,40 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('workers', function (Blueprint $table) {
+        Schema::create("workers", function (Blueprint $table) {
             $table->id();
+
+            $table->string("icard", 20); // Identification Card
+            $table->string("fname", 100); // First name
+            $table->string("mname", 100)->nullable(); // Middle name
+            $table->string("lname", 100); // Last name
+            $table->string("mlname", 100)->nullable(); // Second last name
+            $table->string("photo", 255)->nullable();
+            $table->string("phone", 20)->nullable();
+            $table->string("email", 100)->nullable();
+            $table->string("title")->nullable(); // Job title
+
+            $table
+                ->foreignId("company_id")
+                ->nullable()
+                ->constrained("companies")
+                ->onUpdate("cascade")
+                ->onDelete("set null");
+
+            $table
+                ->foreignId("city_id")
+                ->nullable()
+                ->constrained("cities")
+                ->onUpdate("cascade")
+                ->onDelete("set null");
+
+            $table->softDeletes();
             $table->timestamps();
         });
     }
@@ -22,6 +47,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('workers');
+        Schema::dropIfExists("workers");
     }
 };
