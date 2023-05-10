bffb51c8ac1acf964ebe9a254f0f09a59cc4fb8b
Christian <nihilist.cc@gmail.com>
2023-05-10 10:34:37 -0500

    update City model

--- imsc-server-test/app/Models/City.php
+++ imsc-server-test/app/Models/City.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class City extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ["name"];
+    // protected $hidden = ["created_at", "updated_at", "deleted_at"];
+    protected $dates = ["created_at", "updated_at", "deleted_at"];
 }
