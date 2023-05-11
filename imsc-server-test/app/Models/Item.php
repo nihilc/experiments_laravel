32eb1c2f16097713a27cfbc8c821a92f3c41c0a4
Christian <nihilist.cc@gmail.com>
2023-05-11 17:29:23 -0500

    update Item model

--- imsc-server-test/app/Models/Item.php
+++ imsc-server-test/app/Models/Item.php
@@ -4,8 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Item extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ["cod", "state", "image"];
+    // protected $hidden = [];
+    protected $dates = ["created_at", "updated_at", "deleted_at"];
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function city()
+    {
+        return $this->belongsTo(City::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+    public function attributes()
+    {
+        return $this->belongsToMany(Attribute::class)->withPivot("value");
+    }
 }
