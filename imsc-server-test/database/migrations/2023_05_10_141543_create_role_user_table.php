7a45959bacecbb07f9d317bcf8c5a7116f3c92fa
Christian <nihilist.cc@gmail.com>
2023-05-11 13:43:01 -0500

    update Role and Role_User migrations

--- imsc-server-test/database/migrations/2023_05_10_141543_create_role_user_table.php
+++ imsc-server-test/database/migrations/2023_05_10_141543_create_role_user_table.php
@@ -4,15 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('role_user', function (Blueprint $table) {
+        Schema::create("role_user", function (Blueprint $table) {
             $table->id();
+            $table
+                ->foreignId("role_id")
+                ->constrained("roles")
+                ->onDelete("cascade")
+                ->onUpdate("cascade");
+            $table
+                ->foreignId("user_id")
+                ->constrained("users")
+                ->onDelete("cascade")
+                ->onUpdate("cascade");
             $table->timestamps();
         });
     }
@@ -22,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('role_user');
+        Schema::dropIfExists("role_user");
     }
 };
