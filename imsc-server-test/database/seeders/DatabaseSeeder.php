441076682126646ab2eb8172cc77824419c8ab8c
Christian <nihilist.cc@gmail.com>
2023-05-11 13:45:13 -0500

    update seeder add Role and Role_User

--- imsc-server-test/database/seeders/DatabaseSeeder.php
+++ imsc-server-test/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,11 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Company;
+use App\Models\City;
+use App\Models\Worker;
+use App\Models\User;
+use App\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,13 +17,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-        \App\Models\Company::factory(5)->create();
-        \App\Models\City::factory(5)->create();
-        \App\Models\Worker::factory(30)->create();
-        \App\Models\User::factory(5)->create();
+        Company::factory(5)->create();
+        City::factory(5)->create();
+        Worker::factory(30)->create();
+        Role::create(["name" => "Admin"]);
+        Role::create(["name" => "Inventory"]);
+        User::factory()
+            ->count(5)
+            ->create()
+            ->each(function ($user) {
+                $user->roles()->attach(Role::inRandomOrder()->first());
+            });
     }
 }
