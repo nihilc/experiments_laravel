fcf810b00c47cb7351056b98612d2e9b28c57377
Christian <nihilist.cc@gmail.com>
2023-05-11 15:09:25 -0500

    update Attribute model

--- imsc-server-test/app/Models/Attribute.php
+++ imsc-server-test/app/Models/Attribute.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Attribute extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ["name"];
+    // protected $hidden = ["deleted_at"];
+    protected $dates = ["deleted_at"];
+
+    public $timestamps = false;
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
