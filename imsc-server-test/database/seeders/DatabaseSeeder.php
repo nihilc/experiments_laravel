db3afa2f71776b7262090f1328356dc04b986ca1
Christian <nihilist.cc@gmail.com>
2023-05-11 13:54:52 -0500

    update Category factory and Seeder

--- imsc-server-test/database/seeders/DatabaseSeeder.php
+++ imsc-server-test/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 use App\Models\Company;
 use App\Models\City;
@@ -28,5 +29,6 @@ class DatabaseSeeder extends Seeder
             ->each(function ($user) {
                 $user->roles()->attach(Role::inRandomOrder()->first());
             });
+        Category::factory(10)->create();
     }
 }
