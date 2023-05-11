d604812367f7a931de56db080ca827cf15d4f839
Christian <nihilist.cc@gmail.com>
2023-05-11 15:08:53 -0500

    update Attribute migration

--- imsc-server-test/database/migrations/2023_05_10_141713_create_attributes_table.php
+++ imsc-server-test/database/migrations/2023_05_10_141713_create_attributes_table.php
@@ -4,16 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('attributes', function (Blueprint $table) {
+        Schema::create("attributes", function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string("name");
+            $table
+                ->foreignId("category_id")
+                ->nullable()
+                ->constrained("categories")
+                ->onUpdate("cascade")
+                ->onDelete("set null");
+            $table->softDeletes();
         });
     }
 
@@ -22,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('attributes');
+        Schema::dropIfExists("attributes");
     }
 };
