0133e2ffe0b6b92e79e281ba325524ad0334a397
Christian <nihilist.cc@gmail.com>
2023-05-12 17:38:12 -0500

    update Assignment migrations

--- imsc-server-test/database/migrations/2023_05_10_141850_create_assignments_table.php
+++ imsc-server-test/database/migrations/2023_05_10_141850_create_assignments_table.php
@@ -4,15 +4,36 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('assignments', function (Blueprint $table) {
+        Schema::create("assignments", function (Blueprint $table) {
             $table->id();
+            $table->enum("type", [
+                "Initial Assignment",
+                "Reassignment",
+                "Admission to Inventory",
+                "Re-admission to Inventory",
+                "Maintenance",
+                "Removal",
+            ]);
+            $table->date("date");
+            $table->note("note");
+
+            $table
+                ->foreignId("worker_id")
+                ->constrained("workers")
+                ->onDelete("set null")
+                ->onUpdate("cascade");
+            $table
+                ->foreignId("user_id")
+                ->constrained("users")
+                ->onDelete("set null")
+                ->onUpdate("cascade");
+            $table->softDeletes();
             $table->timestamps();
         });
     }
@@ -22,6 +43,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('assignments');
+        Schema::dropIfExists("assignments");
     }
 };
--- imsc-server-test/database/migrations/2023_05_10_141915_create_assignment_item_table.php
+++ imsc-server-test/database/migrations/2023_05_10_141915_create_assignment_item_table.php
@@ -4,15 +4,27 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('assignment_item', function (Blueprint $table) {
+        Schema::create("assignment_item", function (Blueprint $table) {
             $table->id();
+            $table->text("assign_note");
+            $table->text("return_note");
+            $table->date("return_date");
+            $table
+                ->foreignId("assignment_id")
+                ->constrained("assignments")
+                ->onUpdate("cascade")
+                ->onDelete("restrict");
+            $table
+                ->foreignId("item_id")
+                ->constrained("items")
+                ->onUpdate("cascade")
+                ->onDelete("restrict");
             $table->timestamps();
         });
     }
@@ -22,6 +34,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('assignment_item');
+        Schema::dropIfExists("assignment_item");
     }
 };
