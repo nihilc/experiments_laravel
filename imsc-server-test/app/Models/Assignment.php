30414fe01a5477a6693de5c164885df2902dade7
Christian <nihilist.cc@gmail.com>
2023-05-15 10:12:05 -0500

    update Assignment Model

--- imsc-server-test/app/Models/Assignment.php
+++ imsc-server-test/app/Models/Assignment.php
@@ -4,8 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Assignment extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ["type", "date", "note"];
+    // protected $hidden = ["created_at", "updated_at", "deleted_at"];
+    protected $dates = ["created_at", "updated_at", "deleted_at"];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function worker()
+    {
+        return $this->belongsTo(Worker::class);
+    }
+
+    public function items()
+    {
+        return $this->belongsToMany(Item::class)->withPivot([
+            "assign_note",
+            "return_note",
+            "return_date",
+        ]);
+    }
 }
