53f07a79496fc8661de01fefa4f0e00ddbb052b9
Christian <nihilist.cc@gmail.com>
2023-05-15 11:57:06 -0500

    add api Routes

--- imsc-server-test/routes/api.php
+++ imsc-server-test/routes/api.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\AssignmentController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CityController;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\WorkerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +21,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware("auth:sanctum")->get("/user", function (Request $request) {
+//     return $request->user();
+// });
+
+Route::apiResources([
+    "company" => CompanyController::class,
+    "city" => CityController::class,
+    "worker" => WorkerController::class,
+    "user" => UserController::class,
+    "category" => CategoryController::class,
+    "item" => ItemController::class,
+    "assignment" => AssignmentController::class,
+]);
